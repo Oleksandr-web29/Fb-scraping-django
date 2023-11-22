38e447c7b44a286d2243144bdfeccc7a529c8a3c
Oleksandr-web29 <Bohdan.web29@gmail.com>
2023-11-22 15:58:24 +0800

    update backend-views.py

--- Fb_scraping_app/views.py
+++ Fb_scraping_app/views.py
@@ -12,24 +12,24 @@ def index(request):
       
       bot = FacebookScraper()
       try:
-      #    # login
-      #    bot.login(email,passw)
-      #    # sleep
-      #    time.sleep(5)
-      #    # navigate to desired query and obtain the posts
-      #    prof_link = bot.get_page(query)
-      #    bot.navigate(prof_link)
-      #    story_links = []
-      #    time.sleep(5)
-      #    comz = bot.all_comments()
-      #    for coms in comz:
-      #          story_links.append("https://mbasic.facebook.com"+coms)
+         # login
+         bot.login(email,passw)
+         # sleep
+         time.sleep(5)
+         # navigate to desired query and obtain the posts
+         prof_link = bot.get_page(query)
+         bot.navigate(prof_link)
+         story_links = []
+         time.sleep(5)
+         comz = bot.all_comments()
+         for coms in comz:
+               story_links.append("https://mbasic.facebook.com"+coms)
          
-      #    all_comments = []
-      #    # iterate through story_links
-      #    for story in story_links:
-      #          url, name, comment = bot.get_comments_links(story)
-      #          all_comments.append({"url":url, "name":name, "comment": comment})
+         all_comments = []
+         # iterate through story_links
+         for story in story_links:
+               url, name, comment = bot.get_comments_links(story)
+               all_comments.append({"url":url, "name":name, "comment": comment})
          return render(request, 'index.html', {"msg":"success"})
       except:
          return render(request, 'index.html', {"msg":"err"})
